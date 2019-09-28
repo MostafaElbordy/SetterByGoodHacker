cc6f0ccba6107472cff239de13ba374b27fe1d09
MostafaElbordy <52612582+MostafaElbordy@users.noreply.github.com>
2019-09-28 14:05:51 -0400

    Add files via upload

---
+++ GoodHacker.sh
@@ -0,0 +1,182 @@
+#Please Dont Take This Tool Without Putting Our Name
+#This Tool Was Maded By Good Hacker Team
+#colers
+c='\033[1;36m'
+g='\033[1;32m'
+r='\033[1;31m'
+y='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+z='\033[1;80m'
+clear
+
+
+echo -e "$r  ____                 _   _   _            _             "
+sleep 0.2
+echo -e "$z / ___| ___   ___   __| | | | | | __ _  ___| | _____ _ __ "
+sleep 0.2
+echo -e "$c| |  _ / _ \ / _ \ / _  | | |_| |/ _  |/ __| |/ / _ \  __|"
+sleep 0.2
+echo -e "$g| |_| | (_) | (_) | (_| | |  _  | (_| | (__|   <  __/ |   "
+sleep 0.2
+echo -e "$p \____|\___/ \___/ \__,_| |_| |_|\__,_|\___|_|\_\___|_|   "
+sleep 0.2
+echo -e "                                                          "
+sleep 0.2
+
+echo -n -e "$r ------------" ; echo -n -e "$z This tool Was Maded By Good Hacker Team" ; echo -n -e "$r---------"
+sleep 0.2
+echo ''
+echo -n -e "$r ---------------" ; echo -n -e "$z You Tube : https://bit.ly/2nvrRgO" ; echo -n -e "$r------------"
+echo ''
+sleep 0.2
+echo -n -e '$y This Tool Is Maded To Setting Up Termux..'
+echo ''
+sleep 0.2
+echo -n -e '$g This Tool Will Take 20:30 Min '
+echo ''
+sleep 0.2
+echo -n -e '$p Start..!'
+echo ''
+sleep 0.2
+echo -n -e "$p [1]yes or [2]no ? $r===> " ; read a ; echo ''
+echo ''
+sleep 0.2
+if [ $a -eq 1 ];
+ then
+	clear
+	sleep 0.2
+	pkg update
+	sleep 0.2
+	pkg upgrade
+	sleep 0.2
+	pkg install git
+	sleep 0.2
+	pkg install zip
+	sleep 0.2
+	pkg install unzip
+	sleep 0.2
+	pkg install unrar
+	sleep 0.2
+	pkg install figlet
+	sleep 0.2
+	pkg install php
+	sleep 0.2
+	pkg install python
+	sleep 0.2
+	pkg install python2
+	sleep 0.2
+	pkg install python3
+	sleep 0.2
+	pkg install ruby
+	sleep 0.2
+	pkg install bash
+	sleep 0.2
+	pkg install perl
+	sleep 0.2
+	pkg install nmap
+	sleep 0.2
+	pkg install clang
+	sleep 0.2
+	pkg install macchanger
+	sleep 0.2
+	pkg install nano
+	sleep 0.2
+	pkg install figlet
+	sleep 0.2
+	pkg install cowsay
+	sleep 0.2
+	pkg install curl
+	sleep 0.2
+	pkg install tar
+	sleep 0.2
+	pkg install tor
+	sleep 0.2
+	pkg install wget
+	sleep 0.2
+	pkg install wcalc
+	sleep 0.2
+	pkg install openssl
+	sleep 0.2
+	pkg install openssh
+	sleep 0.2
+	pkg install fish
+	sleep 0.2
+	pkg install wget -y
+	sleep 0.2
+	pkg install python2 -y
+	sleep 0.2
+	pkg install python2 -dev -y
+	sleep 0.2
+	pkg install toilet
+	sleep 0.2
+	pkg install wgetrc
+	sleep 0.2
+	pkg install net-tools
+	sleep 0.2
+	pkg install w3m
+	sleep 0.2
+	pkg install proot
+	sleep 0.2
+	echo -n -e 'Complete..?'
+	echo -n -e "[1]Yes or [2]No ?";read v;echo ''
+	echo ''
+	if [ $v -eq 1 ];
+	 then
+		clear
+		apt update
+		apt upgrade
+		apt install php
+		apt install python
+		apt install git
+		apt install goland
+		apt install hast
+		apt install nano
+		apt install hydra
+		apt install havij
+		apt install wireshark
+		apt install cmatrix
+	fi
+	if [ $v -eq 2];
+	 then
+	      	echo 'Thanks You For Downloding Our Tool'
+		echo ''
+		sleep 0.2
+		echo 'By Good Hacker'
+		echo ''
+		sleep 0.2
+		echo -e ' ____             '
+		sleep 0.2
+		echo -e '| __ ) _   _  ___ '
+		sleep 0.2
+		echo -e '|  _ \| | | |/ _ \'
+		sleep 0.2
+		echo -e '| |_) | |_| |  __/'
+		sleep 0.2
+		echo -e '|____/ \__, |\___|'
+		sleep 0.2
+		echo -e '       |___/      '
+		sleep 0.2
+		exit
+	fi
+fi
+if [ $a -eq 2 ];
+ then
+	clear
+	echo -e ' Thanks For Downloading Our Tool.'
+	echo ''
+	sleep 0.2
+	echo -e '  ____             '
+	sleep 0.2
+	echo -e ' | __ ) _   _  ___ '
+	sleep 0.2
+	echo -e ' |  _ \| | | |/ _ \'
+	sleep 0.2
+	echo -e ' | |_) | |_| |  __/'
+	sleep 0.2
+	echo -e ' |____/ \__/ |\___|'
+	sleep 0.2
+	echo -e '        |___/      '
+	sleep 0.2
+	exit
+fi
